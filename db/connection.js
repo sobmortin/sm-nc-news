1db00141523429fa15a64cd833f38c781b025a7c
Sebastian Martin <sebastianmartin@Sebastians-MacBook-Pro.local>
2019-02-01 11:49:54 +0000

    updated package.json and connection

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,12 @@
 const knex = require('knex');
-const dbConfig = require('../knexfile');
+// const config = require('../knexfile');
 
-const connection = knex(dbConfig);
+const ENV = process.env.NODE_ENV || 'development';
+const config =	ENV === 'production'
+	  ? { client: 'pg', connection: process.env.DATABASE_URL }
+	  : require('../knexfile');
+
+const connection = knex(config);
+
+// module.exports = require('knex')(config);
 module.exports = connection;
--- listen.js
+++ listen.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-const PORT = 9090;
+const { PORT = 9090 } = process.env;
 
 app.listen(PORT, (err) => {
   if (err) console.log(err);
